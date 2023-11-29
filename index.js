2fc5e6b72942fe19083bca2c810741c74b8e0cc1
Failur rahuman <100862900+FAILU026@users.noreply.github.com>
2023-11-29 12:27:56 +0530

    add frontend url

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = expres()
 app.use(expres.json());
 app.use(cors(
     {
-        origin: ["http://localhost:3000"],
+        origin: ["https://survey-form-failu026.vercel.app"],
         methods: ["POST", "GET"],
         credentials: true
       }
